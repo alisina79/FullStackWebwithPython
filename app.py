d94ba23ce50a6e61dd2c6dbc6b39759e88659100
alisina79 <116415437+alisina79@users.noreply.github.com>
2022-12-12 17:33:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,145 @@
+from flask import Flask, render_template, url_for, request, redirect, flash, session
+
+#flask with small f represents the library itself
+#Flask with capital F shows the instance/instance varible for the libray
+#render_template is used to return one html file at a time
+#url_for is used to return the page as well but automatically. 
+#WTForms includes (passwordfield, emailfield, textfield)
+#passlib , a library for password. 
+#encryption, decryption  (sha256_crypt) 
+from wtforms import Form, StringField, EmailField, PasswordField, validators
+from passlib.hash import sha256_crypt
+#import mysql.connector
+#from mysql import flask_mysqldb
+from flask_mysqldb import MySQL, MySQLdb
+
+app = Flask(__name__)
+app.secret_key = '123456789'
+
+
+#Flask(__name__) instance function. 
+#parameter __name__ is passed through the instance function
+
+mysql = MySQL()
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'post_data'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql.init_app(app)
+
+
+
+@app.route('/') #it creates a path for the page on the browser
+def home(): #function which returns
+	return render_template('home.html') #using render_template fun, you return one html page at a time
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+
+
+@app.route('/team')
+def team():
+	return render_template('team.html')
+
+class RegisterFrom(Form):
+	name = StringField('Name', [validators.length(min=3, max=40)])
+	username = StringField('Username', [validators.length(min=3, max=20)])
+	email = EmailField('Email', [validators.Email(), validators.length(min=3, max=20)])
+	password = PasswordField('Passsword', [validators.length(min=5)])
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegisterFrom(request.form)
+
+	if request.method == 'POST' and form.validate():
+		name = form.name.data
+		username = form.username.data
+		email = form.email.data
+		password = sha256_crypt.encrypt(str(form.password.data))
+
+		#cursor class
+		cur = mysql.connection.cursor()
+		cur.execute("INSERT INTO users(Name, Username, Email, Password) VALUES(%s, %s,%s,%s)", (name, username, email, password))
+		mysql.connection.commit()
+		cur.close()
+
+		flash('You are regsitered. ', 'success')
+
+		return redirect(url_for('home'))
+
+	return render_template('register.html', form=form)
+
+class LoginForm(Form):
+	username = StringField('Username', [validators.length(min=3, max=20)])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        # GEt user form
+        username = form.username.data
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username=%s", [username])
+
+        if result > 0:
+            # Get stored value
+            data = cur.fetchone()
+            password = data['Password']
+            # Compare password
+            if sha256_crypt.verify(password_candidate, password):
+                # passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+
+            else:
+                flash('Incorrect password', 'danger')
+                return render_template('login.html', form=form)
+
+        else:
+            flash('Username not found', 'danger')
+            # Close connection
+            cur.close()
+            return render_template('login.html', form=form)
+    return render_template('login.html', form=form)
+
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
+
+@app.route('/logout')
+def logout():
+	cur = mysql.connection.cursor()
+	session.clear()
+	flash('you are logged out','success')
+	return redirect(url_for('home'))
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+#Vistual Studio C++ 2015, 2017
+#Database Connectivity 
+#Form, type of forms (WTForms)
+# password, encrypted (unreadable ) - passlib
+#MYSQL, flask-mysqldb, mysql-connector
+
+#render_field 
+
+
+
+
+
+
+
---
+++ postdata.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Nov 29, 2022 at 11:56 AM
+-- Server version: 5.7.34
+-- PHP Version: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `postdata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Id` int(10) NOT NULL,
+  `Name` varchar(50) DEFAULT NULL,
+  `Username` varchar(50) DEFAULT NULL,
+  `Email` varchar(50) DEFAULT NULL,
+  `Password` varchar(150) DEFAULT NULL,
+  `createdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
